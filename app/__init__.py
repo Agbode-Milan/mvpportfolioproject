21aa3f9873b510442a31a1711a272bcbe007591b
Matutozi <sobowalegz2@gmail.com>
2024-05-23 23:10:08 +0100

    changes to bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import bcrypt
+from flask_bcrypt import Bcrypt
 
 # Initialize extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
+bcrypt = Bcrypt()
+
 
 def create_app(config_class='config.Config'):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize extensions with app
     db.init_app(app)
     login_manager.init_app(app)
+    bcrypt.init_app(app)
 
     return app
